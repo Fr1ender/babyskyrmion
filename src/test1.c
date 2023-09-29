1d9605a240f3310fc6dd7b2e0aa97baee5cff20f
Friender <pisces4u@protonmail.ch>
2023-09-29 16:32:42 +0900

    FormFunctionを一応作り終えた

--- src/test1.c
+++ src/test1.c
@@ -35,6 +35,8 @@ typedef struct {
 
 /* User-defined routines */
 
+PetscReal      PotentialTerm(AppCtx *, PetscReal );
+PetscReal      SkyrmTerm(PetscReal ,PetscReal ,PetscReal, PetscReal, PetscReal, AppCtx *);
 PetscErrorCode FormInitialGuess(AppCtx * ,Vec ); /*\phi_1 \phi_2 \phi_3*/
 PetscErrorCode FormFunction(Tao, Vec, PetscReal *, void *);
 PetscReal      FuncComm(PetscReal, PetscReal, PetscReal );
@@ -130,6 +132,25 @@ int main(int argc, char **argv){
   return 0;
 }
 
+/* Evaluate potential term. In this problem, U(\phi) = \phi_3*/
+
+PetscReal Potentialterm(AppCtx *user ,PetscReal phi_3){
+  return -0.5 * user->param_c0 * phi_3 * phi_3;
+}
+
+/* for evaluating skyrm term
+  NOT USE THIS
+PetscReal SkyrmTerm(PetscReal entry1, PetscReal entry2l, PetscReal entry2r, PetscReal entry3l, PetscReal entry3r, AppCtx *user){
+  PetscReal    nx = user->hx, ny = user->hy;
+  PetscReal    dp2dx,dp3dy;
+  
+  dp2dx = (entry2r - entry2l) / nx;
+  dp3dy = (entry3r - entry3l) / ny;
+
+  return entry1 * dp2dx * dp3dy;
+}
+*/
+
 /*
   FormInitialGuess
   (X Y Z) is (\phi_1 \phi_2 \phi_3)
@@ -204,7 +225,7 @@ PetscErrorCode FormFunction(Tao tao, Vec X, PetscReal *f,void *ptr )
 
   AppCtx            *user = (AppCtx *)ptr;
   PetscReal          hx = user->hx, hy = user->hy, area, three = 3.0, p5 = 0.5;
-  PetscReal          zero = 0.0, vb, vl, vr, vt, dp1dx, dp1dy,dp2dx,dp2dy,dp3dx,dp3dy, flin = 0.0, fquad = 0.0;
+  PetscReal          zero = 0.0, vb, vl, vr, vt, dp1dx, dp1dy,dp2dx,dp2dy,dp3dx,dp3dy, flin = 0.0, fquad = 0.0, f12 = 0.0 ,fskyrm = 0.0, fpot = 0.0;
   PetscReal          v;//, cdiv3 = user->param / three;
   const PetscScalar *x;
   PetscInt           nx = user->mx, ny = user->my, i, j, k1,k2,k3;
@@ -215,16 +236,22 @@ PetscErrorCode FormFunction(Tao tao, Vec X, PetscReal *f,void *ptr )
   PetscCall(VecGetArrayRead(X,&x));
   dim = nx * ny;
 
-  /* Compute function */
+  /* Compute function 
+
+    Evaluate integrate by summing value at volume of two triangular prism
+  
+  */
+
+  /* lower triangle */
   for (j = -1; j < ny; j++) {
     for (i = -1; i < nx; i++) {
       k1  = nx * j + i;
       k2  = nx * j + i + dim;
       k3  = nx * j + i + dim * 2;
       /* ||\nabla /phi_1||^2 */
-      v  = zero;
-      vr = zero;
-      vt = zero;
+      v  = 1.0;
+      vr = 1.0;
+      vt = 1.0;
       if (i >= 0 && j >= 0) v = x[k1];
       if (i < nx - 1 && j > -1) vr = x[k1 + 1];
       if (i > -1 && j < ny - 1) vt = x[k1 + nx];
@@ -243,6 +270,7 @@ PetscErrorCode FormFunction(Tao tao, Vec X, PetscReal *f,void *ptr )
       dp2dy = (vt - v) / hy;
       fquad += dp2dx * dp2dx + dp2dy * dp2dy;
 
+
       /* ||\nabla /phi_3||^2 */
       v  = zero;
       vr = zero;
@@ -252,27 +280,75 @@ PetscErrorCode FormFunction(Tao tao, Vec X, PetscReal *f,void *ptr )
       if (i > -1 && j < ny - 1) vt = x[k3 + nx];
       dp3dx = (vr - v) / hx;
       dp3dy = (vt - v) / hy;
+
       fquad += dp3dx * dp3dx + dp3dy * dp3dy;
-      fquad = user->param_c2 * fquad / 2.0;
+      // fquad = user->param_c2 * fquad / 2.0; after summing all, do this
+
+      f12 = x[k1]*dp2dx*dp3dy + x[k3]*dp1dx*dp2dy + x[k2]*dp3dx*dp1dy;
+      f12 -= x[k2]*dp1dx*dp3dy + x[k3]*dp2dx*dp1dy + x[k1]*dp3dx*dp2dy;
+      fskyrm += f12 * f12;
+      f12 = 0.0;
+
+      fpot += PotentialTerm(user, x[k3]);
+
       // flin -= (v + vr + vt) / 3.0 ;
     }
   }
-  for( j = -1; j < ny; j++){
-    for(i = -1; i < nx; i++){
+  /* upper triangle */
+  for (j = 0; j <= ny; j++) {
+    for (i = 0; i <= nx; i++) {
       k1  = nx * j + i;
-      vb = zero;
-      vl = zero;
-      v  = zero;
+      k2  = nx * j + i + dim;
+      k3  = nx * j + i + dim * 2;
+      /* ||\nabla /phi_1||^2 */
+      vb = 1.0;
+      vl = 1.0;
+      v  = 1.0;
       if (i < nx && j > 0) vb = x[k1 - nx];
       if (i > 0 && j < ny) vl = x[k1 - 1];
       if (i < nx && j < ny) v = x[k1];
-      dvdx  = (v - vl) / hx;
-      dvdy  = (v - vb) / hy;
-      fquad = fquad + dvdx * dvdx + dvdy * dvdy;
-      flin  = flin - (vb + vl + v) / 3.0;
+      dp1dx = (v - vl) / hx;
+      dp1dy = (v - vb) / hy;
+      fquad += dp1dx * dp1dx + dp1dy * dp1dy;
+
+      /* ||\nabla /phi_2||^2 */
+      vb = zero;
+      vl = zero;
+      v  = zero;
+      if (i < nx && j > 0) vb = x[k2 - nx];
+      if (i > 0 && j < ny) vl = x[k2 - 1];
+      if (i < nx && j < ny) v = x[k2];
+      dp2dx = (v - vl) / hx;
+      dp2dy = (v - vb) / hy;
+      fquad += dp2dx * dp2dx + dp2dy * dp2dy;
+
+
+      /* ||\nabla /phi_3||^2 */
+      vb = zero;
+      vl = zero;
+      v  = zero;
+      if (i < nx && j > 0) vb = x[k3 - nx];
+      if (i > 0 && j < ny) vl = x[k3 - 1];
+      if (i < nx && j < ny) v = x[k3];
+      dp3dx = (v - vl) / hx;
+      dp3dy = (v - vb) / hy;
+      fquad += dp3dx * dp3dx + dp3dy * dp3dy;
+
+      f12 = x[k1]*dp2dx*dp3dy + x[k3]*dp1dx*dp2dy + x[k2]*dp3dx*dp1dy;
+      f12 -= x[k2]*dp1dx*dp3dy + x[k3]*dp2dx*dp1dy + x[k1]*dp3dx*dp2dy;
+      fskyrm = f12*f12;
+      f12 = 0;
+      fpot += PotentialTerm(user, x[k3]);
     }
   }
 
+  fquad  = user->param_c2 * fquad / 2.0;
+  fskyrm = p5 * user->param_c4 * fskyrm;
+
+  PetscCall(VecRestoreArrayRead(X, &x));
+
+  area = p5 * hx * hy;
+  *f   = area * (fquad + fskyrm + fpot);
 
   PetscFunctionReturn(PETSC_SUCCESS);
 }
