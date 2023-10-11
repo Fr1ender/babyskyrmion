581ccdb43e7f751ec91082c91baed87ddbf39668
Friender <pisces4u@protonmail.ch>
2023-10-11 17:58:54 +0900

    change filename and add gradient

--- src/speight.c
+++ src/speight.c
@@ -16,6 +16,7 @@ See Juha Jaykka and Martin Speight PHYSICAL REVIEW D 82, 125030 (2010) and PETSc
 PETSc library has many example for numerical problem. "eptorsion1.c", which is one of the examples is very helpful for me to code this program.
 
 */
+#include "petscsystypes.h"
 #include "petsctao.h"
 #include "petscvec.h"
 
@@ -66,10 +67,10 @@ int main(int argc, char **argv){
 
 
   /* Specify default parameters for the problem, check for command-line overrides */
-  user.param_c0 = 1.0;
-  user.param_c2 = 1.0;
-  user.param_c4 = 1.0;
-  user.lambda   = 1.0;
+  user.param_c0 = one;
+  user.param_c2 = one;
+  user.param_c4 = one;
+  user.lambda   = one;
   user.region   = 10.0;
   PetscCall(PetscOptionsGetInt(NULL, NULL, "-my", &my, &flg));
   PetscCall(PetscOptionsGetInt(NULL, NULL, "-mx", &mx, &flg));
@@ -90,7 +91,7 @@ int main(int argc, char **argv){
 
 
   /* Allocate vectors */
-  PetscCall(VecCreateSeq(PETSC_COMM_SELF, user.ndim*3, &user.y)); // *3 
+  PetscCall(VecCreateSeq(PETSC_COMM_SELF, user.ndim*3, &user.y)); // *3 for three fields
   PetscCall(VecDuplicate(user.y, &user.s));
   PetscCall(VecDuplicate(user.y, &x));
   /*
@@ -222,8 +223,8 @@ PetscErrorCode FormFunction(Tao tao, Vec X, PetscReal *f,void *ptr )
 {
 
   AppCtx            *user = (AppCtx *)ptr;
-  PetscReal          hx = user->hx, hy = user->hy, area, three = 3.0, p5 = 0.5;
-  PetscReal          zero = 0.0, vb, vl, vr, vt, dp1dx, dp1dy,dp2dx,dp2dy,dp3dx,dp3dy, flin = 0.0, fquad = 0.0, f12 = 0.0 ,fskyrm = 0.0, fpot = 0.0;
+  PetscReal          hx = user->hx, hy = user->hy, area, p5 = 0.5;
+  PetscReal          zero = 0.0, vb, vl, vr, vt, dp1dx, dp1dy,dp2dx,dp2dy,dp3dx,dp3dy, fquad = 0.0, f12 = 0.0 ,fskyrm = 0.0, fpot = 0.0;
   PetscReal          v;//, cdiv3 = user->param / three;
   const PetscScalar *x;
   PetscInt           nx = user->mx, ny = user->my, i, j, k1,k2,k3;
@@ -357,7 +358,7 @@ PetscErrorCode FormGradient(Tao tao, Vec X, Vec G, void *ptr){
   PetscInt          nx = user->mx, ny = user->my, dim, i,j,k1,k2,k3,ind;
   PetscReal         hx = user->hx, hy = user->hy;
   PetscReal         vb, vl, vr, vt,v;
-  PetscReal         dp1dx, dp1dy,dp2dx,dp2dy,dp3dx,dp3dy, flin = 0.0, fquad = 0.0, f12 = 0.0 ,fskyrm = 0.0, fpot = 0.0, area;
+  PetscReal         dp1dx, dp1dy,dp2dx,dp2dy,dp3dx,dp3dy,f12 = 0.0, area;
   const PetscScalar *x;
 
   PetscFunctionBeginUser;
@@ -381,7 +382,6 @@ PetscErrorCode FormGradient(Tao tao, Vec X, Vec G, void *ptr){
       if (i > -1 && j < ny - 1) vt = x[k1 + nx];
       dp1dx = (vr - v) / hx;
       dp1dy = (vt - v) / hy;
-      fquad += dp1dx / hx + dp1dy / hy;
 
       /* ||\nabla /phi_2||^2 */
       v  = zero;
@@ -392,8 +392,6 @@ PetscErrorCode FormGradient(Tao tao, Vec X, Vec G, void *ptr){
       if (i > -1 && j < ny - 1) vt = x[k2 + nx];
       dp2dx = (vr - v) / hx;
       dp2dy = (vt - v) / hy;
-      fquad += dp2dx / hx + dp2dy / hx;
-
 
       /* ||\nabla /phi_3||^2 */
       v  = zero;
@@ -405,38 +403,185 @@ PetscErrorCode FormGradient(Tao tao, Vec X, Vec G, void *ptr){
       dp3dx = (vr - v) / hx;
       dp3dy = (vt - v) / hy;
 
-      fquad += dp3dx / hx + dp3dy / hy;
-
       f12 = x[k1]*dp2dx*dp3dy + x[k3]*dp1dx*dp2dy + x[k2]*dp3dx*dp1dy;
       f12 -= x[k2]*dp1dx*dp3dy + x[k3]*dp2dx*dp1dy + x[k1]*dp3dx*dp2dy;
-      fskyrm += f12 * f12;
-      f12 = 0.0;
 
-      fpot += PotentialTerm(user, x[k3]);
-      /* unfinished
+      // following variable is derivative of skyrm term
+      PetscReal ddeddp1dx,ddeddp1dy,ddeddp2dx,ddeddp2dy,ddeddp3dx,ddeddp3dy;
+      PetscReal dedp1,dedp2,dedp3;
+      ddeddp1dx = (user->param_c4 * f12 * (x[k3] * dp2dy - x[k2] * dp3dy) ) / hx;
+      ddeddp1dy = (user->param_c4 * f12 * (x[k2] * dp2dx - x[k3] * dp2dy) ) / hy;
+      ddeddp2dx = (user->param_c4 * f12 * (x[k1] * dp3dy - x[k3] * dp1dy) ) / hx;
+      ddeddp2dy = (user->param_c4 * f12 * (x[k3] * dp3dy - x[k1] * dp3dy) ) / hy;
+      ddeddp3dx = (user->param_c4 * f12 * (x[k2] * dp1dy - x[k1] * dp2dy) ) / hx;
+      ddeddp3dy = (user->param_c4 * f12 * (x[k1] * dp2dy - x[k2] * dp1dy) ) / hy;
+      dedp1     =  user->param_c4 * f12 * (dp2dx * dp3dy - dp3dx * dp2dy);
+      dedp2     =  user->param_c4 * f12 * (dp3dx * dp1dy - dp1dx * dp3dy);
+      dedp3     =  user->param_c4 * f12 * (dp1dx * dp2dy - dp2dx * dp1dy);
+      /* phi_1 */
       if (i != -1 && j != -1) {
         ind = k1;
-        val = -dvdx / hx - dvdy / hy - cdiv3;
+        val = -dp1dx / hx - dp1dy / hy - ddeddp1dx - ddeddp1dy - dedp1 ;
         PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
       }
       if (i != nx - 1 && j != -1) {
         ind = k1 + 1;
-        val = dvdx / hx - cdiv3;
+        val = dp1dx / hx + ddeddp1dx - dedp1 ;
         PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
       }
       if (i != -1 && j != ny - 1) {
         ind = k1 + nx;
-        val = dvdy / hy - cdiv3;
+        val = dp1dy / hy + ddeddp1dy - dedp1 ;
         PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
       }
-      */
+      /* phi_2 */
+      if (i != -1 && j != -1) {
+        ind = k2;
+        val = -dp2dx / hx - dp2dy / hy - ddeddp2dx - ddeddp2dy - dedp2;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != nx - 1 && j != -1) {
+        ind = k2 + 1;
+        val = dp2dx / hx + ddeddp2dx - dedp2;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != -1 && j != ny - 1) {
+        ind = k2 + nx;
+        val = dp2dy / hy + ddeddp2dy - dedp2;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      /* phi_3 */
+      if (i != -1 && j != -1) {
+        ind = k3;
+        val = -dp3dx / hx - dp3dy / hy - ddeddp3dx - ddeddp3dy - dedp3 - user->param_c0 * x[k3];
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != nx - 1 && j != -1) {
+        ind = k3 + 1;
+        val = dp3dx / hx + ddeddp3dx - dedp3 - user->param_c0 * x[k3];
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != -1 && j != ny - 1) {
+        ind = k3 + nx;
+        val = dp3dy / hy + ddeddp3dy - dedp3 - user->param_c0 * x[k3];
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
     }
-  } 
+  }
+
+  /* upper triangle*/
+
+  for (j = 0; j <= ny; j++) {
+    for (i = 0; i <= nx; i++) {
+      k1  = nx * j + i;
+      k2  = nx * j + i + dim;
+      k3  = nx * j + i + dim * 2;
+      /* ||\nabla /phi_1||^2 */
+      vb = 1.0;
+      vl = 1.0;
+      v  = 1.0;
+      if (i < nx && j > 0) vb = x[k1 - nx];
+      if (i > 0 && j < ny) vl = x[k1 - 1];
+      if (i < nx && j < ny) v = x[k1];
+      dp1dx = (v - vl) / hx;
+      dp1dy = (v - vb) / hy;
+
+      /* ||\nabla /phi_2||^2 */
+      vb = zero;
+      vl = zero;
+      v  = zero;
+      if (i < nx && j > 0) vb = x[k2 - nx];
+      if (i > 0 && j < ny) vl = x[k2 - 1];
+      if (i < nx && j < ny) v = x[k2];
+      dp2dx = (v - vl) / hx;
+      dp2dy = (v - vb) / hy;
+
+
+      /* ||\nabla /phi_3||^2 */
+      vb = zero;
+      vl = zero;
+      v  = zero;
+      if (i < nx && j > 0) vb = x[k3 - nx];
+      if (i > 0 && j < ny) vl = x[k3 - 1];
+      if (i < nx && j < ny) v = x[k3];
+      dp3dx = (v - vl) / hx;
+      dp3dy = (v - vb) / hy;
+
+      f12 = x[k1]*dp2dx*dp3dy + x[k3]*dp1dx*dp2dy + x[k2]*dp3dx*dp1dy;
+      f12 -= x[k2]*dp1dx*dp3dy + x[k3]*dp2dx*dp1dy + x[k1]*dp3dx*dp2dy;
+
+      // following variable stands derivative of skyrm term
+      PetscReal ddeddp1dx,ddeddp1dy,ddeddp2dx,ddeddp2dy,ddeddp3dx,ddeddp3dy;
+      PetscReal dedp1,dedp2,dedp3;
+      ddeddp1dx = (user->param_c4 * f12 * (x[k3] * dp2dy - x[k2] * dp3dy) ) / hx;
+      ddeddp1dy = (user->param_c4 * f12 * (x[k2] * dp2dx - x[k3] * dp2dy) ) / hy;
+      ddeddp2dx = (user->param_c4 * f12 * (x[k1] * dp3dy - x[k3] * dp1dy) ) / hx;
+      ddeddp2dy = (user->param_c4 * f12 * (x[k3] * dp3dy - x[k1] * dp3dy) ) / hy;
+      ddeddp3dx = (user->param_c4 * f12 * (x[k2] * dp1dy - x[k1] * dp2dy) ) / hx;
+      ddeddp3dy = (user->param_c4 * f12 * (x[k1] * dp2dy - x[k2] * dp1dy) ) / hy;
+      dedp1     =  user->param_c4 * f12 * (dp2dx * dp3dy - dp3dx * dp2dy);
+      dedp2     =  user->param_c4 * f12 * (dp3dx * dp1dy - dp1dx * dp3dy);
+      dedp3     =  user->param_c4 * f12 * (dp1dx * dp2dy - dp2dx * dp1dy);
+      /* phi_1 */
+      if (i != nx && j != 0) {
+        ind = k1 - nx;
+        val = -dp1dy / hy - ddeddp1dy - dedp1;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != 0 && j != ny) {
+        ind = k1 - 1;
+        val = -dp1dx / hx - ddeddp1dx - dedp1;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != nx && j != ny) {
+        ind = k1;
+        val = dp1dx / hx + dp1dy / hy + ddeddp1dx + ddeddp1dx - dedp1;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+
+      /* phi_2 */
+
+      if (i != nx && j != 0) {
+        ind = k2 - nx;
+        val = -dp2dy / hy - ddeddp2dy - dedp2;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != 0 && j != ny) {
+        ind = k2 - 1;
+        val = -dp2dx / hx - ddeddp2dx - dedp2;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != nx && j != ny) {
+        ind = k2;
+        val = dp2dx / hx + dp2dy / hy + ddeddp2dx + ddeddp2dx - dedp2;
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+
+      /* phi_3 */
+
+      if (i != nx && j != 0) {
+        ind = k3 - nx;
+        val = -dp3dy / hy - ddeddp3dy - dedp3 - user->param_c0 * x[k3];
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != 0 && j != ny) {
+        ind = k3 - 1;
+        val = -dp3dx / hx - ddeddp3dx - dedp3 - user->param_c0 * x[k3];
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+      if (i != nx && j != ny) {
+        ind = k3;
+        val = dp3dx / hx + dp3dy / hy + ddeddp3dx + ddeddp3dx - dedp3 - user->param_c0 * x[k3];
+        PetscCall(VecSetValues(G, 1, &ind, &val, ADD_VALUES));
+      }
+    }
+  }  
+   
   /* Assemble Vector */
   PetscCall(VecAssemblyBegin(G));
   PetscCall(VecAssemblyEnd(G));
 
   area = p5 * hx * hy;
-
+  PetscCall(VecScale(G, area));
   PetscFunctionReturn(PETSC_SUCCESS);
 }
